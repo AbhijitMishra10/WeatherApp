504c93c47ec2a9c8536152b02b08bac16fec164e
AbhijitMishra10 <abhijitmishra2000@gmail.com>
2025-07-09 12:59:19 +0530

    changed the apiId, api url

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 // A basic weather function which takes a parameter 'city' and returns a weather promise, either in the form of a resolved(success) one or rejected(failure) one
+const apiKey = 'd446d6d7fbf84c0abde63545250907'
+
 function weather(city) {
     return new Promise((resolve,reject) => {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=85510a4d4a383a5d17351e4c5aab24a3&units=metric`)
+        const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5&aqi=no&alerts=no`
+        fetch(url)
             .then((response) => {
                 if(!response.ok) {
                     reject('Data cannot be fetched');
@@ -23,7 +26,7 @@ let unresolveMsg = () => console.log('Oopsie!😞, Couldnt fetch your data');
 function searchCity() {
     let city = document.getElementById("cityInput").value.trim()
     if(city === "") {
-        alert('Enter a city name')
+        alert('Pleas enter a city name')
         return
     }
 
@@ -31,33 +34,36 @@ function searchCity() {
         .then((data) => {
             resolveMsg()
             console.log(data);
+
+            const current = data.current
+            const location = data.location
+            const astro = data.forecast.forecastday[0].astro
             document.getElementById("day").textContent = new Date().toDateString()
-            document.getElementById("cityName").textContent = `Weather in ${data.name}`
-            document.getElementById("clouds").textContent = data.clouds.all
-            document.getElementById("long").textContent = data.coord.lon
-            document.getElementById("lat").textContent = data.coord.lat
-            document.getElementById("temp").textContent = data.main.temp 
-            document.getElementById("feels_like").textContent = data.main.feels_like           
-            document.getElementById("humidity").textContent = data.main.humidity
-            document.getElementById("wind").textContent = data.wind.speed
-            document.getElementById("gust").textContent = data.wind.gust
-            document.getElementById("degree").textContent = data.wind.deg
-            document.getElementById("rain").textContent = data.rain?.['1h'] 
-                                                          ?? data.rain?.['24h'] 
-                                                          ?? '0'
-            document.getElementById("country").textContent = data.sys.country
-            document.getElementById("sunrise").textContent = new Date(data.sys.sunrise * 1000).toLocaleTimeString()
-            document.getElementById("sunset").textContent = new Date(data.sys.sunset * 1000).toLocaleTimeString()
-            document.getElementById("description").textContent = `Description: ${data.weather[0].description}, Main: ${data.weather[0].main}`
+            document.getElementById("cityName").textContent = `Weather in ${data.location.name}`
+            document.getElementById("clouds").textContent = current.cloud
+            document.getElementById("long").textContent = location.lon
+            document.getElementById("lat").textContent = location.lat
+            document.getElementById("temp").textContent = current.temp_c 
+            document.getElementById("feels_like").textContent = current.feelslike_c           
+            document.getElementById("humidity").textContent = current.humidity
+            document.getElementById("wind").textContent = current.wind_kph
+            document.getElementById("gust").textContent = current.gust_kph
+            document.getElementById("degree").textContent = current.wind_degree
+            document.getElementById("rain").textContent = current.precip_mm ?? '0'
+            document.getElementById("country").textContent = location.country
+            document.getElementById("sunrise").textContent = astro.sunrise
+            document.getElementById("sunset").textContent = astro.sunset
+            document.getElementById("description").textContent = `Description: ${current.condition.text}, Main: ${current.condition.text}`
 
             document.getElementById("weatherResult").classList.remove('hidden')
+        
+            getForecast(data.forecast.forecastday)
         })
         .catch((err) => {
             unresolveMsg()
             console.log(err);
             alert('Could not fetch weather report. Try again!!')
         })
-    getForecast(city)
 }
 
 // This function is called when there is need of finding the weather of the current location it takes two callback functions, onSuccess and error, it fetches it when the Current Location button is clicked
@@ -73,41 +79,37 @@ function getLocation() {
 function onSuccess(position) {
     const lat = position.coords.latitude
     const lon = position.coords.longitude
+    const prob =  `${lat}, ${lon}`
 
-    const api = '85510a4d4a383a5d17351e4c5aab24a3'
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}&units=metric`
-
-    fetch(url) 
-        .then(response => {
-            if(!response.ok){
-                throw new Error('Couldnt fetch location')
-            } 
-            return response.json() 
-        })
-        .then(data => {
+    weather(prob) 
+        .then((data) => {
             resolveMsg()
             console.log(data);
 
+            const current = data.current
+            const location = data.location
+            const astro = data.forecast.forecastday[0].astro
+            
             document.getElementById("day").textContent = new Date().toDateString()
-            document.getElementById("cityName").textContent = `Weather in ${data.name}`
-            document.getElementById("clouds").textContent = data.clouds.all
-            document.getElementById("long").textContent = data.coord.lon
-            document.getElementById("lat").textContent = data.coord.lat
-            document.getElementById("temp").textContent = data.main.temp 
-            document.getElementById("feels_like").textContent = data.main.feels_like           
-            document.getElementById("humidity").textContent = data.main.humidity
-            document.getElementById("wind").textContent = data.wind.speed
-            document.getElementById("gust").textContent = data.wind.gust
-            document.getElementById("degree").textContent = data.wind.deg
-            document.getElementById("rain").textContent = data.rain?.['1h'] 
-                                                          ?? data.rain?.['24h'] 
-                                                          ?? '0'
-            document.getElementById("country").textContent = data.sys.country
-            document.getElementById("sunrise").textContent = new Date(data.sys.sunrise * 1000).toLocaleTimeString()
-            document.getElementById("sunset").textContent = new Date(data.sys.sunset * 1000).toLocaleTimeString()
-            document.getElementById("description").textContent = `Description: ${data.weather[0].description}, Main: ${data.weather[0].main}`
+            document.getElementById("cityName").textContent = `Weather in ${data.location.name}`
+            document.getElementById("clouds").textContent = current.cloud
+            document.getElementById("long").textContent = location.lon
+            document.getElementById("lat").textContent = location.lat
+            document.getElementById("temp").textContent = current.temp_c 
+            document.getElementById("feels_like").textContent = current.feelslike_c           
+            document.getElementById("humidity").textContent = current.humidity
+            document.getElementById("wind").textContent = current.wind_kph
+            document.getElementById("gust").textContent = current.gust_kph
+            document.getElementById("degree").textContent = current.wind_degree
+            document.getElementById("rain").textContent = current.precip_mm ?? '0'
+            document.getElementById("country").textContent = location.country
+            document.getElementById("sunrise").textContent = astro.sunrise
+            document.getElementById("sunset").textContent = astro.sunset
+            document.getElementById("description").textContent = `Description: ${current.condition.text}, Main: ${current.condition.text}`
 
             document.getElementById("weatherResult").classList.remove('hidden')
+        
+            getForecast(data.forecast.forecastday)
         })
         .catch((err) => {
             unresolveMsg()
@@ -120,56 +122,29 @@ function error() {
 }
 
 // This function also takes a parameter city, which is provided via the input field and gives the updated weather every 3 hours for the next 5 days
-function getForecast(city) {
-    const Forecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=85510a4d4a383a5d17351e4c5aab24a3&units=metric` 
-
-    fetch(Forecast) 
-        .then((res) => {
-            if(!res.ok) throw new Error('Forecast cannot be fetched')
-            return res.json()    
-        }) 
-        .then((data) => {
-            const forecastList = data.list
-            const dailyForecast = []
-
-            forecastList.forEach(i => {
-                if(i.dt_txt.includes("12:00:00")) {
-                    dailyForecast.push(i)
-                }
-            });
-
-            display(dailyForecast)
-
-        })
-        .catch(err => {
-            console.log('Forecast displaying error', err);
-        })
-
-}
-// This function helps to display the weather for the next 5 days,5 different cards for 5 different days with the dates of the days mentioned (filtered at 12:00:00 pm)
-function display(forecast) {
+function getForecast(forecastList) {
+   
     const forecastDiv = document.getElementById("forecast")
     const forecastCards = document.getElementById("forecastCards")
 
     forecastCards.innerHTML = ""
 
-    forecast.forEach(i => {
-        const date = new Date(i.dt_txt).toDateString()
-        const cloudss = i.clouds['all']
-        const temp = i.main.temp
-        const wind = i.wind.speed
-        const humidity = i.main.humidity
-        const icon = i.weather[0].icon
+    forecastList.forEach(day => {
+        const date = new Date(day.date).toDateString()
+        const cloudss = day.day.daily_chance_of_rain ?? 0
+        const temp = day.day.avgtemp_c
+        const wind = day.day.maxwind_kph
+        const humidity = day.day.avghumidity
+        const icon = day.day.condition.icon
 
         const card = document.createElement("div")
         card.className = "bg-white p-4 rounded shadow text-center"
 
         card.innerHTML = `<h3 class="font-semibold mb-2">${date}</h3>
-        <img src="https://openweathermap.org/img/wn/${icon}@2x.png"
-        class="mx-auto mb-3">       
+        <img src="https:${icon}" class="mx-auto mb-3" alt="icon">       
         <p>Clouds:☁️ ${cloudss}%</p>     
         <p>Temperature: 🌡️ ${temp}°C</p>     
-        <p>Wind:🌬️ ${wind} m/s</p>
+        <p>Wind:🌬️ ${wind} kph</p>
         <p>Humidity:💧 ${humidity}%</p>`
 
         forecastCards.appendChild(card)
